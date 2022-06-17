c2659b9308e79e4bc8fbf40fadc4e53e104c92f5
Pedro Gabriel <pedrogabrielbhz@gmail.com>
2022-06-17 07:48:04 -0300

    Funfte Kommit

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,11 +2,6 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
 import { errorHandler, NotFoundError } from "@pepe_tickets/common";
 
 const app = express();
@@ -21,12 +16,6 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
-
-// Routers
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
